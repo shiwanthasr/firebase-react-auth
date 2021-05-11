d557785486ae09e391f461346639f19b83c90755
Chamath Viranga <chamathviranga@gmail.com>
2021-05-11 15:07:15 +0530

    table modification

--- src/components/authentication/manageUsers.js
+++ src/components/authentication/manageUsers.js
@@ -5,19 +5,58 @@ function Example() {
   const data = React.useMemo(
     () => [
       {
-        col1: "Minsk",
-        col2: "27",
-        col3: "rain",
+        email: "test@gmail.com",
+        insurance_company: "N/A",
+        name: "Test Polcie - Agent",
+        nic: "45785121v",
+        police_branch: "Koswatta",
+        role: "police : officer",
+        action: (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<div class="btn-group" role="group" aria-label="Basic example">
+                                                            <button type="button" class="btn btn-sm btn-secondary" data-toggle="modal" data-target="#exampleModal">Edit</button>
+                                                            <button type="button" class="btn btn-sm btn-danger">Remove</button>
+                                                          </div>`,
+            }}
+          />
+        ),
       },
       {
-        col1: "Vilnius",
-        col2: "30",
-        col3: "rain",
+        email: "test2@gmail.com",
+        insurance_company: "N/A",
+        name: "Test Polcie 2 - Agent",
+        nic: "98625321v",
+        police_branch: "Kotikawaththa",
+        role: "police : officer",
+        action: (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<div class="btn-group" role="group" aria-label="Basic example">
+                                                            <button type="button" class="btn btn-sm btn-secondary">Edit</button>
+                                                            <button type="button" class="btn btn-sm btn-danger">Remove</button>
+                                                          </div>`,
+            }}
+          />
+        ),
       },
       {
-        col1: "London",
-        col2: "23",
-        col3: "rain",
+        email: "test@gmail.com",
+        insurance_company: "Co-Op Insurance (Malabe)",
+        name: "Test insurance - Agent",
+        nic: "58912498v",
+        police_branch: "N/A",
+        role: "Insurance : agent",
+        action: (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<div class="btn-group" role="group" aria-label="Basic example">
+                                                            <button type="button" class="btn btn-sm btn-secondary">Edit</button>
+                                                            <button type="button" class="btn btn-sm btn-danger">Remove</button>
+                                                          </div>`,
+            }}
+          />
+        ),
       },
     ],
     []
@@ -49,6 +88,10 @@ function Example() {
         Header: "Role",
         accessor: "role", // accessor is the "key" in the data
       },
+      {
+        Header: "Action",
+        accessor: "action",
+      },
     ],
     []
   );
@@ -57,67 +100,141 @@ function Example() {
     useTable({ columns, data }, useSortBy);
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-12">
-          <div className="card">
-            <div className="card-body bg-light">
-            <div>
-            <table className="mx-auto" {...getTableProps()} style={{ border: "solid 1px gray" }}>
-              <thead>
-                {headerGroups.map((headerGroup) => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                      <th
-                        {...column.getHeaderProps(
-                          column.getSortByToggleProps()
-                        )}
-                        style={{
-                          borderBottom: "solid 1px gray",
-                          borderRight:"solid 1px gray",
-                          color: "black",
-                          fontWeight: "normal",
-                          padding:"5px",
-                          fontSize:"0.9em"
-                        }}
-                      >
-                        {column.render("Header")}
-                        <span>
-                          {column.isSorted
-                            ? column.isSortedDesc
-                              ? "🔽"
-                              : "🔼"
-                            : ""}
-                        </span>
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                  prepareRow(row);
-                  return (
-                    <tr {...row.getRowProps()}>
-                      {row.cells.map((cell) => {
-                        return (
-                          <td
-                            {...cell.getCellProps()}
+    <div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header bg-dark">
+              <h5 className="modal-title text-light" id="exampleModalLabel">
+                Update System User Info
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+
+              <div className="form-group">
+                  <label className="font-weight-bold">Email : </label>
+                  <input className="form-control" placeholder="Type here .." />
+              </div>
+
+              <div className="form-group">
+                  <label className="font-weight-bold">Insurace Company : </label>
+                  <input className="form-control" placeholder="Type here .." />
+              </div>
+
+              <div className="form-group">
+                  <label className="font-weight-bold">Name : </label>
+                  <input className="form-control" placeholder="Type here .." />
+              </div>
+
+              <div className="form-group">
+                  <label className="font-weight-bold">NIC : </label>
+                  <input className="form-control" placeholder="Type here .." />
+              </div>
+
+              <div className="form-group">
+                  <label className="font-weight-bold">Police Branch : </label>
+                  <input className="form-control" placeholder="Type here .." />
+              </div>
+
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-success">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-header">
+                <span className="font-weight-bold">Mange Users</span>
+              </div>
+              <div className="card-body bg-light">
+                <table
+                  className="mx-auto w-100"
+                  {...getTableProps()}
+                  style={{ border: "solid 1px gray" }}
+                >
+                  <thead>
+                    {headerGroups.map((headerGroup) => (
+                      <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                          <th
+                            {...column.getHeaderProps(
+                              column.getSortByToggleProps()
+                            )}
                             style={{
-                              padding: "10px",
-                              border: "solid 1px gray",
+                              borderBottom: "solid 1px gray",
+                              borderRight: "solid 1px gray",
+                              color: "black",
+                              fontWeight: "normal",
+                              padding: "5px",
+                              fontSize: "0.9em",
                             }}
                           >
-                            {cell.render("Cell")}
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+                            {column.render("Header")}
+                            <span>
+                              {column.isSorted
+                                ? column.isSortedDesc
+                                  ? "🔽"
+                                  : "🔼"
+                                : ""}
+                            </span>
+                          </th>
+                        ))}
+                      </tr>
+                    ))}
+                  </thead>
+                  <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                      prepareRow(row);
+                      return (
+                        <tr {...row.getRowProps()}>
+                          {row.cells.map((cell) => {
+                            return (
+                              <td
+                                {...cell.getCellProps()}
+                                style={{
+                                  padding: "10px",
+                                  border: "solid 1px gray",
+                                }}
+                              >
+                                {cell.render("Cell")}
+                              </td>
+                            );
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 ReactDOM.render(
   <React.StrictMode>
