b97a3c498cbb800ee3b7aeebd981200b3d8deeca
Chamath Viranga <chamathviranga@gmail.com>
2021-05-12 06:14:37 +0530

    remove delete model

--- src/components/authentication/InsuranceReports.js
+++ src/components/authentication/InsuranceReports.js
@@ -172,47 +172,6 @@ const InsuranceReports = () => {
 
       {/* Delete Model */}
 
-      <div
-        className="modal fade"
-        id="deleteModal"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="deleteModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header bg-dark">
-              <h5 className="modal-title text-light" id="deleteModalLabel">
-                Delete Insurance Report
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              Are you sure you want to delete this Report ?
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-danger">
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
       {/* Table */}
       <div className="container mt-4">
@@ -274,8 +233,6 @@ const InsuranceReports = () => {
                             id="submit"
                             type="button"
                             className="btn btn-danger"
-                            data-toggle="modal"
-                            data-target="#deleteModal"
                             onClick={_delete}
                           >
                             Delete
